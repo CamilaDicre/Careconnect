c5d24fd749464f0a942226e6fce6b348b375e004
AmethCruz <amethcruz65@gmail.com>
2025-07-08 10:08:39 -0500

    Buenas, algunos cambios son para que me pudiera arreglar el footer, el de componets, settings, y lo demás fueron cambios en el tipo de letra del dashboard y contact

--- js/components.js
+++ js/components.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d00cf54a1d29db8648a719cf1408093b389ec981
 async function loadComponent(elementId, componentPath, callback) {
     try {
         const response = await fetch(componentPath);
@@ -16,7 +12,7 @@ async function loadComponent(elementId, componentPath, callback) {
 // Load components when the page is ready
 document.addEventListener('DOMContentLoaded', function() {
     // Load header, then load login.js and main.js
-    loadComponent('header-component', '/components/header.html', () => {
+    loadComponent('header-component', 'components/header.html', () => {
         const loginScript = document.createElement('script');
         loginScript.src = 'js/login.js';
         document.body.appendChild(loginScript);
@@ -25,32 +21,10 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.appendChild(mainScript);
     });
     // Load footer
-    loadComponent('footer-component', '/components/footer.html');
+    loadComponent('footer-component', 'components/footer.html');
 
     if (window.location.pathname.endsWith('login.html')) {
         var loginSignupBtn = document.getElementById('loginSignupButton');
         if (loginSignupBtn) loginSignupBtn.style.display = 'none';
     }
-<<<<<<< HEAD
-=======
-async function loadComponent(elementId, componentPath) {
-    try {
-        const response = await fetch(componentPath);
-        const html = await response.text();
-        document.getElementById(elementId).innerHTML = html;
-    } catch (error) {
-        console.error('Error loading component:', error);
-    }
-}
-
-// Load components when the page is ready
-document.addEventListener('DOMContentLoaded', function() {
-    // Load header
-    loadComponent('header-component', 'components/header.html');
-    
-    // Load footer
-    loadComponent('footer-component', 'components/footer.html');
->>>>>>> 9d7b88c1af6013c69bd288d56a3395a7d5a8f659
-=======
->>>>>>> d00cf54a1d29db8648a719cf1408093b389ec981
 }); 
